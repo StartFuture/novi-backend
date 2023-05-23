f13c9bd35b13b3bc8dce74364c91d9c005d20ae1
Gabriel Cecconi <gabriel.cecconi25@gmail.com>
2023-05-22 21:13:28 -0300

    Fiz as alterações no arquivo tables.sql fazendo algumas mudanças nas tabelas address e user

--- tables.sql
+++ tables.sql
@@ -1,6 +1,6 @@
 use novi;
 
-create table table_address (
+create table address (
     id_address int(11) not null AUTO_INCREMENT,
     cep varchar(8),
     state_user varchar(2),
@@ -11,20 +11,21 @@ create table table_address (
     PRIMARY KEY (id_address)    
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-create table table_users (
+create table user (
     id_user int(11) not null AUTO_INCREMENT,
+    id_address varchar(255),
     name_user varchar(255),
     last_name varchar(255),
     date_birth date,
     email varchar(30),
     cpf varchar(11),
     cellphone varchar(30),
-    id_address varchar(255),
     password_user varchar(255),
     news boolean,
     info_conditions boolean,
     share_data boolean
-    PRIMARY KEY (id_user)
+    PRIMARY KEY (id_user),
+    FOREIGN KEY(id_address) REFERENCES address (id_address)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*
