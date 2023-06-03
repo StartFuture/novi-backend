df5ff33c145d26ad403d17beaca5ae9dfaab2e01
giovana <giovanadealmeida1105@gmail.com>
2023-06-03 15:31:05 -0300

    Add dao_table

--- app/dao/dao_table.py
+++ app/dao/dao_table.py
@@ -27,10 +27,10 @@ def table_accommodation(id, travel_destination, travel_style, accommodation_styl
     )
 
     insert_accommodation = """
-    INSERT (id, travel_destination, travel_style, accommodation_style, is_country, warn, mild, cold, price, details, local_name) 
-    INTO accomodation 
+    INSERT INTO accomodation 
+    (id, travel_destination, travel_style, accommodation_style, is_country, warn, mild, cold, price, details, local_name) 
     VALUES
-    (default, {travel_destination}, {travel_style}, {accommodation_style}, {is_country}, {warn}, {mild}, {cold}, {price}, '{details}', {local_name});    
+    (default, {atravel_destination}, {travel_style}, {accommodation_style}, {is_country}, {warn}, {mild}, {cold}, {price}, '{details}', {local_name});    
     """
 
 
@@ -43,8 +43,8 @@ def table_transport(id, night_style, music_preference, building_preference, trad
     )
 
     insert_tour = """
-    INSERT (id, night_style, music_preference, building_preference, tradicion_preference, party_preference, water_preference, walk_preference, historic_preference, sport_preference, food_preference, id_accommodation, price, details)
-    INTO tour
+    INSERT INTO tour
+    (id, night_style, music_preference, building_preference, tradicion_preference, party_preference, water_preference, walk_preference, historic_preference, sport_preference, food_preference, id_accommodation, price, details)
     VALUES
     (default, {night_style}, {music_preference}, {building_preference}, {tradicion_preference}, {party_preference}, {water_preference}, {historic_preference}, {sport_preference}, {food_preference}, {id_accommodation}, {price}, {details})
     """
@@ -59,8 +59,8 @@ def table_transport(id, details, price, transport_style):
     )
 
     insert_transport = """
-    INSERT (id, details, price, transport_style)
-    INTO transport
+    INSERT INTO transport
+    (id, details, price, transport_style)
     VALUES
     (default, {details}, {price}, {transport_style})
     """
