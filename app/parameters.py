fdecb1545b5b1396bc0233362e995ad6fca09841
Ian Soares <80067490+Ian-Soares@users.noreply.github.com>
2023-06-14 17:16:16 -0300

    fix(db_connection): Update parameters.py
    Fix DB Connection with DB_PORT from string to integer.

--- app/parameters.py
+++ app/parameters.py
@@ -7,7 +7,7 @@ HOST = os.getenv("DB_HOST")
 USER = os.getenv("DB_USER")
 PASSWORD = os.getenv("DB_PASSWORD")
 DATABASE = os.getenv("DB_NAME")
-PORT = os.getenv("DB_PORT")
+PORT = int(os.getenv("DB_PORT"))
 
 JWT_SECRET = os.getenv("JWT_SECRET")
 JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
